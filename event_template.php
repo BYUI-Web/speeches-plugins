c5342e1556bfc85ae903fb5b1c789ccb7d6e5234
Crowther, Josh <jshcrowthe@gmail.com>
2014-03-17 09:47:53 -0600

    Added Related by Speaker Functionality

--- event_template.php
+++ event_template.php
@@ -7,10 +7,48 @@ $post_time;
 $prep_material;
 $current_post_type;
 $current_post_tags;
+
+function getPostsBySpeaker($current_post_id) {
+	$speakerPosts = array();
+	$counter = 0;
+	$current_post_presenters = get_post_meta($current_post_id, 'presenters')[0];
+
+	if ($current_post_presenters)
+		$current_post_presenters = explode(', ', $current_post_presenters);
+
+	$loop = new WP_Query( array( 'post_type' => 'devotional') );
+	if ($loop->have_posts()) { 
+		while ($loop->have_posts()) {
+			$loop->the_post();
+			$post_id = get_the_ID();
+			$loop_presenters = get_post_meta(get_the_ID(),'presenters')[0];
+			if ($loop_presenters)
+				$loop_presenters = explode(', ', $loop_presenters);
+			foreach ($loop_presenters as $test) {
+				foreach ($current_post_presenters as $comp) {
+					if ($test == $comp) {
+						if ($post_id != $current_post_id) {
+							$add = true;
+							foreach ($speakerPosts as $toAdd) {
+								if ($toAdd == $post_id)
+									$add = false;
+							}
+							if ($add)
+								array_push($speakerPosts, $post_id);
+						}
+					}
+				}
+			}
+			if (count($speakerPosts) == 2)
+				break;
+		}
+	}
+	return $speakerPosts;
+}
 ?>
 <div class="row">
 	<div class="col-xs-12 col-sm-8">
-		
+
 
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<?php $meta = get_post_meta( get_the_ID() ); ?>
@@ -102,8 +140,10 @@ $current_post_tags;
 						<p><?php echo wpautop($meta['transcript'][0]); ?></p>
 					</div>
 					<div id="discussion">
-						
+
 					</div>
+					<br>
+					<a href="#top" class="speeches-button">Back to Top</a>
 				</div>
 			</div>
 			<!-- <?php comments_template(); ?> -->
@@ -148,66 +188,73 @@ $current_post_tags;
 		<div class="sidebar-inner sidebar-featured-speeches">
 			<h2>Related Speeches</h2>
 			<h3>By this Speaker</h3>
-			<hr>
-			<h3>On this Topic</h3>
-			<?php
-			$sameTopicPosts = array();
-			$counter = 0;
-			$loop = new WP_Query( array( 'post_type' => $current_post_type) );
-			if ($loop->have_posts()) { 
-				while ($loop->have_posts()) { 
-					$loop->the_post();
-					$tags = get_the_tags();
+			<?php 
+			$posts = getPostsBySpeaker($current_post); 
+			foreach ($posts as $post) : ?>
+			<a href="<?php the_permalink(); ?>"><h4><?php the_title(); ?></h4></a>
+			<p><?php echo get_the_title($meta['presenters'][0]); ?></p>
+			<p class="meta"><?php echo get_post_meta($meta['presenters'][0], 'title')[0]; ?></p>
+		<?php endforeach; ?>
+		<hr>
+		<h3>On this Topic</h3>
+		<?php
+		$sameTopicPosts = array();
+		$counter = 0;
+		$loop = new WP_Query( array( 'post_type' => $current_post_type) );
+		if ($loop->have_posts()) { 
+			while ($loop->have_posts()) { 
+				$loop->the_post();
+				$tags = get_the_tags();
 
-					foreach ($tags as $tag) {
-						$sameTopic = false;
-						foreach ($current_post_tags as $tagTest) {
-							if ($tag->name == $tagTest->name) {
-								$counter++;
-								if (!(get_the_ID() == $current_post)) {
-									$toAdd = get_the_ID();
-									$exists = false;
-									foreach ($sameTopicPosts as $id) {
-										if ($id == $toAdd)
-											$exists = true;
-									}
-									if (!$exists) :
-										if (count($sameTopicPosts) < 2)
-											array_push($sameTopicPosts, $toAdd);
-										endif;
-									}
-									continue;
+				foreach ($tags as $tag) {
+					$sameTopic = false;
+					foreach ($current_post_tags as $tagTest) {
+						if ($tag->name == $tagTest->name) {
+							$counter++;
+							if (!(get_the_ID() == $current_post)) {
+								$toAdd = get_the_ID();
+								$exists = false;
+								foreach ($sameTopicPosts as $id) {
+									if ($id == $toAdd)
+										$exists = true;
 								}
-								if ($sameTopic)
-									break;
+								if (!$exists) :
+									if (count($sameTopicPosts) < 2)
+										array_push($sameTopicPosts, $toAdd);
+									endif;
+								}
+								continue;
 							}
-						} 
-					}
+							if ($sameTopic)
+								break;
+						}
+					} 
 				}
+			}
 
-				?>
-				<?php foreach ($sameTopicPosts as $post) : ?>
-					<a href="<?php the_permalink(); ?>"><h4><?php the_title(); ?></h4></a>
-					<p><?php echo get_the_title($meta['presenters'][0]); ?></p>
-					<p class="meta"><?php echo get_post_meta($meta['presenters'][0], 'title')[0]; ?></p>
-				<?php endforeach; ?>
-				<hr>
-				<h3>Other <?php echo $current_post_type; ?>s</h3>
-				<?php
-				$loop = new WP_Query( array( 'post_type' => $current_post_type) );
-				while ( $loop->have_posts() ) : $loop->the_post(); 
-				$meta = get_post_meta(get_the_ID());
-				if (get_the_ID() == $current_post)
-					continue;
-				?>
-
+			?>
+			<?php foreach ($sameTopicPosts as $post) : ?>
 				<a href="<?php the_permalink(); ?>"><h4><?php the_title(); ?></h4></a>
 				<p><?php echo get_the_title($meta['presenters'][0]); ?></p>
 				<p class="meta"><?php echo get_post_meta($meta['presenters'][0], 'title')[0]; ?></p>
+			<?php endforeach; ?>
+			<hr>
+			<h3>Other <?php echo $current_post_type; ?>s</h3>
+			<?php
+			$loop = new WP_Query( array( 'post_type' => $current_post_type) );
+			while ( $loop->have_posts() ) : $loop->the_post(); 
+			$meta = get_post_meta(get_the_ID());
+			if (get_the_ID() == $current_post)
+				continue;
+			?>
 
-			<?php endwhile; ?>
-		</div>
-	</aside>
+			<a href="<?php the_permalink(); ?>"><h4><?php the_title(); ?></h4></a>
+			<p><?php echo get_the_title($meta['presenters'][0]); ?></p>
+			<p class="meta"><?php echo get_post_meta($meta['presenters'][0], 'title')[0]; ?></p>
+
+		<?php endwhile; ?>
+	</div>
+</aside>
 </div>
 
 <?php get_footer(); ?>
