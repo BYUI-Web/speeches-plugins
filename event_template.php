b28de62da41219d73d28ea8dadc228471286bd12
Matthew Sessions <hyphrend@matthew.(none)>
2014-03-17 13:09:55 -0600

    Added speaker page as well as linking from speakers pages to devotionals pages and the reverse

--- event_template.php
+++ event_template.php
@@ -180,7 +180,7 @@ function getPostsBySpeaker($current_post_id) {
 						$string = get_post_meta($person, 'speaker_bio')[0];
 						$string = substr($string, 0, strpos(wordwrap($string, 150), "\n"));
 						?>
-						<p class="sidebar-speaker-meta"><?php echo $string; ?> <a class="read-more" href="#">More</a></p>
+						<p class="sidebar-speaker-meta"><?php echo $string; ?> <a class="read-more" href="<?php echo get_permalink($person); ?>">More</a></p>
 					</div>
 				</div>
 			<?php endforeach; ?>
---
+++ speaker_template.php
@@ -0,0 +1,73 @@
+<?php get_header(); ?>
+
+<?php
+
+function getPostsBySpeaker($id) {
+	$speakerPosts = array();
+	$counter = 0;
+
+	$loop = new WP_Query( array( 'post_type' => 'devotional') );
+        while ($loop->have_posts()) {
+                $loop->the_post();
+                $post_id = get_the_ID();
+                $loop_presenters = get_post_meta(get_the_ID(),'presenters')[0];
+                if ($loop_presenters) {
+                    $loop_presenters = explode(', ', $loop_presenters);
+                    
+                    if (in_array($id, $loop_presenters)) {
+                        array_push($speakerPosts, $post_id);
+                    }
+
+                }
+	}
+	return $speakerPosts;
+}
+
+/*
+ * Variables for this post
+ */
+
+    $id = get_the_ID();
+    $speakerName = get_the_title();
+    $image = wp_get_attachment_image_src(get_post_thumbnail_id())[0];
+    $bio = get_post_meta($id, "speaker_bio")[0];
+    
+    $speaker_posts = getPostsBySpeaker($id);
+        
+?>
+
+<div class="row speaker-wrapper">
+    <div class="col-xs-12 col-md-3 speaker-image">
+        <img src="<?php echo $image; ?>" alt="<?php echo $speakerName; ?>'s Image"/>
+    </div>
+    <div class="col-xs-12 col-md-5 speaker-bio-wrapper">
+        <div class="speaker-name">
+            <h2>
+                <?php echo $speakerName; ?>
+            </h2>
+        </div>
+        <div class="speaker-bio">
+            <?php echo $bio; ?>
+        </div>
+    </div>
+    <aside class="col-xs-12 col-md-4 speaker-speeches">
+        <div class="sidebar-inner">
+            <h2 class="speaker-speeches-header">From This Speaker</h2>
+            <ul class="speaker-speeches-list">
+                <?php foreach ($speaker_posts as $post): ?>
+                    <?php
+                        $post_title = get_the_title($post);
+                        $post_date = get_post_meta($post, "event_date")[0];
+                        $url = get_permalink($post);
+                    ?>
+                <li>
+                    <a href="<?php echo $url; ?>" class="speaker-post-title"><?php echo $post_title; ?></a>
+                    <p class="speaker-post-date"><?php echo $post_date; ?></p>
+                </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </aside>
+</div>
+
+<?PHP get_footer();
